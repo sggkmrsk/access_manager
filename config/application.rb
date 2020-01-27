a0f6f21e3ab4b605738ab0a085af9bb102171ac4
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-01-27 18:50:56 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module AccessManager
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
   end
 end
